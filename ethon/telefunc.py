d1477f65171ce9d000905e147963729294999a47
mr-coder-v4 <130746838+mr-coder-v4@users.noreply.github.com>
2023-10-05 10:45:56 +0530

    Update telefunc.py

--- ethon/telefunc.py
+++ ethon/telefunc.py
@@ -50,8 +50,8 @@ async def progress(current, total, event, start, type_of_ps, file=None):
         speed = current / diff
         time_to_completion = round((total - current) / speed) * 1000
         progress_str = "**[{0}{1}]** `| {2}%`\n\n".format(
-            "".join(["🟩" for i in range(math.floor(percentage / 5))]),
-            "".join(["⬜️" for i in range(20 - math.floor(percentage / 5))]),
+            "".join(["🔵" for i in range(math.floor(percentage / 5))]),
+            "".join(["⚪" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2),
         )
         tmp = (
